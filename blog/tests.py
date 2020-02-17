9c1a98ede7de917adc870f3fded27bf0621ab5c3
jordanrob709 <jordanrob709@gmail.com>
2020-02-17 19:35:16 +0300

    added tests

--- blog/tests.py
+++ blog/tests.py
@@ -30,4 +30,18 @@ class BlogTests(TestCase):
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{ self.post.body }', 'nice body content')
-        self.assertEqual(f'{self.post.author}', 'Testuser')
+        self.assertEqual(f'{self.post.author}', 'TestUser')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('post/post.pk/')
+        no_response = self.client.get('post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
