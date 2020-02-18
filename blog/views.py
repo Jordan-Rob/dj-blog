750af256c9a8891c1f44fe55f24432cf342d74a8
jordanrob709 <jordanrob709@gmail.com>
2020-02-18 15:36:20 +0300

    adding delete template and view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
+    Deleteview,
 )
 
 from .models import Post
@@ -32,3 +34,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+class BlogDeleteView(Deleteview):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
