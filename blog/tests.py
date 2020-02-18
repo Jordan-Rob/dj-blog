cca12509a535d7f1ef9697a0daa5c01d87eb9f7a
jordanrob709 <jordanrob709@gmail.com>
2020-02-18 16:41:14 +0300

    adding tests

--- blog/tests.py
+++ blog/tests.py
@@ -22,6 +22,9 @@ class BlogTests(TestCase):
             body='nice body content',
             author=self.user
         )
+        post = self.post
+
+        self.post_id = post.pk
 
     def test_string_representation(self):
         post = Post(title='a simple title')
@@ -39,9 +42,32 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_post_detail_view(self):
-        response = self.client.get('post/1/')
+
+        response = self.client.get('/post/{}/' .format(self.post_id))
+        #response = self.client.get('/post/1/')
         no_response = self.client.get('post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'testing',
+            'body': 'gibberish',
+            'author': self.user,
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'testing')
+
+    def test_post_edit_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'testing edited ',
+            'body': ' gibberish testing'
+        })
+
+        self.assertEqual(response.status_code, 302)
